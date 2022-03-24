3242816fcb715086957c907550ad0434d7999363
william <chenut.william@gmail.com>
2022-03-24 13:39:11 +0100

    dateTime

--- src/main/scala/com/project/spark/model/Message.scala
+++ src/main/scala/com/project/spark/model/Message.scala
@@ -1,30 +1,34 @@
 package com.project.spark.model
+import java.time.LocalDateTime
 
 sealed class Message
 
 class Report(
               peaceWatcher: PeaceWatcher,
               citizens : List[Citizen],
-              wordsHeard : List[String]
+              wordsHeard : List[String],
+              time : String
               ) extends Message {
 
   override def toString(): String = {
     "peaceWatcher : " + peaceWatcher.id.toString + "\n" +
     "location : " + peaceWatcher.location.toString + "\n"+
     "citizen : " + citizens.map(c => c.name + " : score =" + c.peaceScore.value) + "\n " +
-    "words heard : " + wordsHeard
-
+    "words heard : " + wordsHeard + "\n " +
+    "time : " + time
   }
 
 }
 class Alert (
             peaceWatcher: PeaceWatcher,
-            citizen : Citizen
+            citizen : Citizen,
+            time : String,
             ) extends Message {
   override def toString: String = {
     "\t\t Alert ! Alert ! \n"+
     "peaceWatcher : " + peaceWatcher.id.toString + "\n" +
     "location : " + peaceWatcher.location.toString + "\n"+
-    " citizen " + citizen.name + " : score = " + citizen.peaceScore.value
+    " citizen " + citizen.name + " : score = " + citizen.peaceScore.value + "\n " +
+      "time : " + time
   }
 }
--- src/main/scala/com/project/spark/service/MessageService.scala
+++ src/main/scala/com/project/spark/service/MessageService.scala
@@ -3,6 +3,8 @@ package com.project.spark.service
 import com.project.spark.model.{Alert, Citizen, Message, PeaceWatcher, Report}
 import org.apache.spark.rdd.RDD
 
+import java.time.LocalDateTime
+
 class MessageService {
 
   private val wordsDataset = scala.io.Source.fromFile("resources/words.txt", "utf-8")
@@ -10,16 +12,20 @@ class MessageService {
     .flatMap( l => l.split(" ", -1))
     .toList
 
+  private def dateTimeNow(): String ={
+    LocalDateTime.now().toString.replace("T","_").replace("-","").replace(":","")
+  }
+
   def generateMessage(citizens : List[Citizen], peaceWatchers: PeaceWatcher) : List[Message] = {
     val nCitizen = scala.util.Random.nextInt(4)
     val citizenForMessage = scala.util.Random.shuffle(citizens).take(nCitizen)
     if( citizenForMessage.filter(c => c.peaceScore.isBad()).size > 0){
       citizenForMessage.filter(c => c.peaceScore.isBad())
-        .map( c => new Alert(peaceWatchers, c) )
+        .map( c => new Alert(peaceWatchers, c,dateTimeNow()) )
     }else {
       val nWords = scala.util.Random.nextInt(10)
       val wordsForMessage = scala.util.Random.shuffle(wordsDataset).take(nWords)
-      List(new Report(peaceWatchers, citizenForMessage, wordsForMessage))
+      List(new Report(peaceWatchers, citizenForMessage, wordsForMessage,dateTimeNow()))
     }
   }
 
